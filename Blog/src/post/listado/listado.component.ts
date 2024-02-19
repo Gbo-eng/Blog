a8434255a0457e2cc00e4555eb1756b791eab112
Gbo-eng <64713692+Gbo-eng@users.noreply.github.com>
2024-02-19 19:45:48 +0000

    Co

--- Blog/src/post/listado/listado.component.ts
+++ Blog/src/post/listado/listado.component.ts
@@ -1,12 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PostService } from '../post.service';
+import { Post } from '../post';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-listado',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterLink],
   templateUrl: './listado.component.html',
-  styleUrl: './listado.component.css'
+  styles: ``
 })
 export class ListadoComponent {
+  posts: Post[] = [];
+  startIndex: number = 0; 
 
+  constructor(private postService: PostService) {}
+
+  ngOnInit(): void {
+    this.getPosts();
+  }
+
+  getPosts(): void {
+    this.postService.getAll().subscribe(posts => (this.posts = posts));
+  }
+
+  borrar(post: Post): void {
+    this.postService.delete(post.id).subscribe(() => {
+      this.posts = this.posts.filter(p => p.id !== post.id);
+    });
+  }
+
+  siguiente(): void {
+    if (this.startIndex + 3 < this.posts.length) {
+      this.startIndex += 3;
+    }
+  }
+
+  anterior(): void {
+    if (this.startIndex >= 3) {
+      this.startIndex -= 3;
+    }
+  }
 }
